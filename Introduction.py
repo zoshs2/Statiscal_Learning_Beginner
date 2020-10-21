a0e5f79ca5a10086dfd7a41a00dc40d678a76d01
Yungi <zoshs27@gmail.com>
2020-10-21 23:54:31 +0900

    Introduction.py

---
+++ Introduction.py
@@ -0,0 +1,17 @@
+### Introduction
+# Basic statistic for beginners
+
+# 1. Histogram
+# 2. Outliers
+# 3. Box Plot
+# 4. Summary Statistics
+# 5. CDF (Cumulative Distribution Function?)
+# 6. Effect Size
+# 7. Relationship Between Variables
+# 8. Correlation
+# 9. Covariance 
+# 10. Pearson Correlation
+# 11. Spearman's Rank Correlation
+# 12. Mean VS. Median
+# 13. Hypothesis Testing
+# 14. Normal(Gaussian) Distribution & z-score
